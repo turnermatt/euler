c104701b290bed0e13f2b3a78562546d47882733
Matt Turner <mturner@buddy.(none)>
2009-03-11 15:00:17 -0500

    007 faster

--- src/007.py
+++ src/007.py
@@ -4,6 +4,7 @@ Created on Mar 11, 2009
 @author: mturner
 '''
 from time import clock
+import util
 import math
 
 start = clock()
@@ -23,3 +24,23 @@ while len(primes) < 10001:
 finish = clock()
 print primes[len(primes)-1]
 print "Took ", finish - start, " seconds"
+
+start2 = clock()
+limit = 10001
+count = 1
+candidate = 1
+while count < limit:
+    candidate += 2
+    if util.is_prime(candidate):
+        count += 1
+        
+print candidate
+
+finish2 = clock()
+print "Second run took ", finish2 - start2, " seconds"
+
+start3 = clock()
+p = util.Primes()
+print p[10000]
+finish3 = clock()
+print "Third run took ", finish3 - start3, " seconds"
---
+++ src/util.py
@@ -0,0 +1,48 @@
+'''
+Created on Mar 11, 2009
+
+@author: mturner
+'''
+from math import sqrt, floor
+
+def is_prime(n):
+    if n<=1:
+        return False
+    if n<4: # 2 & 3 are prime
+        return True
+    if n%2 == 0:
+        return False
+    if n<9: # already excluded 4,6,8
+        return True
+    if n%3 == 0:
+        return False
+    
+    # All primes greater than 3 can be written in the form 6k+/-1
+    # So we can start at 5 and increment by 6
+    r = floor(sqrt(n))
+    f = 5
+    while f <= r:
+        if n%f == 0 or n%(f+2) == 0: 
+            return False
+        f += 6
+    return True
+
+class Primes:
+    def __getitem__(self, n):
+        '''Get the nth prime number (indexed from zero)'''
+        if n == 0: return 2     
+        limit = n
+        idx = 0
+        candidate = 1
+        while (idx < limit):
+            candidate += 2
+            if is_prime(candidate):
+                idx += 1
+        return candidate
+        
+        
+        
+
+    
+    
+      

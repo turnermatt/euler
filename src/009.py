396680f2ab4ed4ab82ac50b9e8a1083d49f8bb95
Matt Turner <mturner@buddy.(none)>
2009-03-11 13:58:37 -0500

    009 faster

--- src/009.py
+++ src/009.py
@@ -10,14 +10,20 @@ start = clock()
 
 # First try using euclid's formula + brute force
 # generate them using m
+done = False
 for m in range(1000):
-    for n in range(m):
-        a = 2*m*n
-        b = m**2 - n**2
-        c = m**2 + n**2
-        
-        if a**2 + b**2 == c**2 and a+b+c==1000:
-            print a*b*c 
+    if not done:
+        for n in range(m):
+            a = 2*m*n
+            b = m**2 - n**2
+            c = m**2 + n**2
+            
+            if a**2 + b**2 == c**2 and a+b+c==1000:
+                print a*b*c
+                done = True
+                break
+    else:
+        break
 
 
 
